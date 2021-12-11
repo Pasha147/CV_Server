e7cd4d1f589e2052665b5367c25ed2e41b516a42
Pasha <75729255+Pasha147@users.noreply.github.com>
2021-12-11 13:30:43 +0200

    Messages will seved on the file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const nodemailer = require("nodemailer");
+const { sendFile } = require("express/lib/response");
 
 const servapi = "/api/data";
 
@@ -10,85 +12,68 @@ const app = express();
 app.use(express.json());
 
 //==========================================
-let db = [];
 
-async function sendDb(db) {
-  // let testEmailAccount = await nodemailer.createTestAccount();
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "pasha147223s@gmail.com",
-      pass: "@P1471472v",
-    },
-  });
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "pasha147223s@gmail.com",
+    pass: "@P1471472v",
+  },
+});
 
-  let letter = db
-    .map((item, index) => {
-      const str = `${item.name} \n ${item.date} \n ${item.text}\n\n`;
-      return str;
-    })
-    .join(" ");
-
+async function sendF() {
   await transporter.sendMail({
     from: "pasha147223s",
     to: "pasha147223@gmail.com",
     subject: "Node js",
-    text: letter,
+    text: "",
+    attachments: [{ filename: "text.txt", path: "./text.txt" }],
+  });
+  const date = new Date();
+  const dateStr =
+    `${date.getFullYear()}.${date.getMonth()}.` +
+    `${date.getDate()} ${date.getHours()}:` +
+    `${date.getMinutes()}:${date.getSeconds()}`;
+
+  fs.writeFile("./text.txt", dateStr, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("File has been refreshed");
   });
 }
-//============================================
 
 // это всё код middleware
 app.use((req, res, next) => {
   res.append("Access-Control-Allow-Origin", "*"); // разрешает принимать запросы со всех доменов
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // какие методы в запросах разрешается принимать
-  res.append(
-    "Access-Control-Allow-Headers",
-    "Origin, Content-Type" // X-Requested-With, Content-Type, Accept"
-  );
-  // res.append("Access-Control-Max-Age", "2");
-  // res.append("Access-Control-Allow-Credentials", "true");
-  // res.append("Vary", "Origin");
+  res.append("Access-Control-Allow-Headers", "Origin, Content-Type");
+
   next();
 });
-
-//GET
-// app.get(servapi, (req, res) => {
-//   console.log("server get>>");
-//   //   console.log("req.params>>", req.params.id);
-//   //   console.log("req.body>>", req.body);
-//   let data = { a: 2, b: 4 };
-//   res.json(data);
-// });
 
 //POST
 app.post("/api/data/:id", (req, res) => {
   // console.log("Serv POST params>>", req.params.id);
   console.log("Serv POST body", req.body);
-  // res.setHeader();
   let response = "";
   if (req.body.name === "Pasha147") {
-    if (req.body.text === "HiBy") {
-      console.log(`Hi Pasha! You have ${db.length} messages`);
-      response = `You have ${db.length} messages`;
-    }
     if (req.body.text === "HiBySend") {
-      console.log(`Hi Pasha! To send messages`);
-      response = `You have ${db.length} messages`;
-      sendDb(db);
+      console.log(`Hi Pasha! Send messages`);
+      sendF();
     }
   } else {
-    if (db.length > 200) {
-      db = db.slice(100, db.length);
-    }
-    db.push(req.body);
-    response = `N:${db.length} The message from ${req.body.name} was received at ${req.body.date}`;
+    const str = `\n${req.body.date} ${req.body.name}>>> ${req.body.text}`;
+    fs.appendFile("./text.txt", str, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Seved to file");
+    });
   }
 
   res.status(200).json(response);
 });
-
-// app.use(express.static(path.resolve(__dirname, "frontend")));
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
